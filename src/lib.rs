6b5135e176b3c769579c21ad80896e960d5d769d
Dang <zeroed32@protonmail.com>
2023-04-14 19:28:51 -0300

    removed some funcs,added more performance

--- src/lib.rs
+++ src/lib.rs
@@ -12,16 +12,16 @@ pub struct IdentGen<'a> {
     table: &'a [char],
 }
 
-/// All characters that are in the `snake_case` standard.
+/// All characters that are in the lower `snake_case` ascii standard.
 pub const DEFAULT_TABLE: &'static [char] = &[
     'a', 'b', 'c', 'd', 'f', 'e', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
     't', 'u', 'v', 'w', 'x', 'z', '_'
 ];
 
-/// All characters that are in the upper `SNAKE_CASE` standard.
+/// All characters that are in the upper `SNAKE_CASE` ascii standard.
 pub const UPPER_SNAKE: &'static [char] = &[
-    'A', 'B', 'C', 'D', 'F', 'E', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
-    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Z', '_'
+    'A', 'B', 'C', 'D', 'F', 'E', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+    'T', 'U', 'V', 'W', 'X', 'Z', '_'
 ];
 
 impl<'a> IdentGen<'a> {
@@ -56,36 +56,25 @@ impl<'a> IdentGen<'a> {
         }
     }
 
-    /// Returns the underlying character table used in [`Self::advance`] and [`Self::regress`].
+    /// Convenience for `self.advance(1)`.
+    pub fn next(&mut self) -> &str {
+        self.advance(1)
+    }
+
+    /// Convenience for `self.advance(-1)`.
+    pub fn prev(&mut self) -> &str {
+        self.advance(-1)
+    }
+
+    /// Returns the underlying character table used in [`Self::advance`].
     #[inline]
     pub fn table(&self) -> &[char] {
         self.table
     }
 
-    /// Convenience to `self.advance(1)`.
-    #[inline]
-    pub fn next(&mut self) -> &str {
-        self.advance(1)
-    }
-
-    /// Convenience to `self.regress(1)`.
-    #[inline]
-    pub fn prev(&mut self) -> &str {
-        self.regress(1)
-    }
-
-    /// Returns `self.advance(i)` if `i.is_positive()` otherwise `self.regress(i.abs())`.
-    #[inline]
-    pub fn letter_by(&mut self, i: isize) -> &str {
-        if i.is_positive() {
-            self.advance(i as usize)
-        } else {
-            self.regress(i.abs() as usize)
-        }
-    }
-
-    /// Gives you the `i`th possible combination since the one you got,modifying the underlying
-    /// `String` and returning a `&str`.
+    /// In the case is positive gives you the `i`th possible combination since the one you got,
+    /// and in the case of negative gives you the `i`th possible combination prior the one you got,
+    ///modifying the underlying `String` and returning a `&str`.
     /// 
     /// # Examples
     /// 
@@ -96,116 +85,47 @@ impl<'a> IdentGen<'a> {
     /// 
     /// assert_eq!(gen.advance(1), "a");
     /// assert_eq!(gen.advance(2), "c");
-    /// 
-    /// gen.clear();
-    /// 
-    /// assert_eq!(gen.advance(3), "c");
+    /// assert_eq!(gen.advance(-1), "b");
     /// ```
-    pub fn advance(&mut self, i: usize) -> &str {
-            let Self { ref mut ident, table } = *self;
-
-            if i == 0 {
-                return self
-            }
-
-            if ident.is_empty()  {
-                ident.push(table[0]);
-                self.advance(i - 1);
-                self
+    #[inline]
+    pub fn advance(&mut self, i: isize) -> &str {
+        if self.ident.len() == 0 {
+            if i > 0 {
+                self.ident.push(*self.table.first().unwrap());
+                self.advance(i - 1)
             } else {
+                ""
+            }
+        } else if i == 0 {
+            &self.ident
+        } else {
+            let a = get_last_char(&self.ident);
+            let x = self.table.iter().copied().position(|y| a == y).unwrap();
 
-                let mut index = 0;
-                let mut n = table.iter().copied().position(|e| ident.starts_with(e)).unwrap_or(0) + i;
-                let mut next_char = next_char_boundary(&ident, index);
-                let mut len = next_char.unwrap_or(ident.len());
+            let y = i + x as isize;
 
-                while n >= table.len() {
-                    unsafe { replace_char(ident, index, len, *table.last().unwrap()); }
-                    let diff = n - table.len() + 1;
-
-                    if let Some(len_previous) = next_char {
-
-                        index = len_previous;
-
-                        next_char = next_char_boundary(&ident, index);
-                        len = next_char.unwrap_or(ident.len());
-
-                        let s = &ident[index..];
-
-                        if s == "" {
-                            continue
-                        }
- 
-                        n = table.iter().copied().position(|e| s.starts_with(e)).unwrap_or(0) + diff;
-                    } else {
-                        ident.push(table[0]);
-                        self.advance(diff - 1);
-                        return self
-                    }
+            if i < 0 {
+                if y < 0 {
+                    self.ident.pop();
+                    self.advance(i)
+                } else {
+                    let t = self.table[y as usize];
+                    replace_last_char(&mut self.ident, t);
+                    &self.ident 
                 }
-
-                unsafe { replace_char(ident, index, len, table[n]); }
-                self
-            }
-    }
-
-    /// Gives you the `i`th possible combination prior the one you got,modifying the underlying
-    /// `String` and returning a `&str`,being empty if you past the first one.
-    /// 
-    /// # Examples
-    /// 
-    /// ```
-    /// use ident_gen::IdentGen;
-    /// 
-    /// let mut gen = IdentGen::default();
-    /// 
-    /// assert_eq!(gen.advance(1), "a");
-    /// assert_eq!(gen.regress(1), "");
-    /// assert_eq!(gen.advance(2), "b");
-    /// assert_eq!(gen.regress(1), "a");
-    /// ```
-    pub fn regress(&mut self, i: usize) -> &str {
-            let Self { ref mut ident, table } = *self;
-
-            if i == 0 {
-                return self
-            }
-
-            let i = i as isize;
-
-            if ident.is_empty()  {
-                self
+            } else if y >= self.table.len() as isize {
+                let mut t = *self.table.last().unwrap();
+                replace_last_char(&mut self.ident, t);
+                t = *self.table.first().unwrap();
+                self.ident.push(t);
+                let tlen = self.table.len();
+                self.advance(y - tlen as isize) 
             } else {
-                let mut n = table.iter().copied().position(|e| ident.ends_with(e)).unwrap_or(0) as isize - i;
-                let mut prev_char = prev_char_boundary(&ident, ident.len());
-                let mut index = prev_char.unwrap_or(0);
-                let mut len = ident.len();
-
-                while n < 0 {
-                    ident.pop();
-
-                    if let Some(len_previous) = prev_char {
-
-                        index = len_previous;
-
-                        prev_char = prev_char_boundary(&ident, index);
-                        len = prev_char.unwrap_or(0);
-
-                        let s = &ident[..len];
-
-                        if s == "" {
-                            break
-                        }
- 
-                        n = table.iter().copied().position(|e| s.ends_with(e)).unwrap_or(0) as isize + n;
-                    } else {
-                        break
-                    }
-                }
-
-                unsafe { replace_char(ident, index, len, table[n as usize]); }
-                self
+                let t = self.table[y as usize];
+                replace_last_char(&mut self.ident, t);
+                &self.ident
             }
+        }
     }
 
     /// Convenience for `self.ident.clear()`.
@@ -239,33 +159,6 @@ impl<'a> Default for IdentGen<'a> {
     }
 }
 
-#[inline]
-fn next_char_boundary(s: &str, index: usize) -> Option<usize> {
-    char_boundary(s, index, true)
-}
-
-#[inline]
-fn prev_char_boundary(s: &str, index: usize) -> Option<usize> {
-    char_boundary(s, index, false)
-}
-
-#[inline]
-fn char_boundary(s: &str, mut index: usize, forward: bool) -> Option<usize> {
-    let f = if forward { usize::checked_add } else { usize::checked_sub };
-
-    loop {
-        index = f(index, 1)?;        
-
-        if s.len() < index {
-            break None
-        }
-
-        if s.is_char_boundary(index) {
-            break Some(index)
-        } 
-    }
-}
-
 use core::fmt::{self, Debug, Display, Formatter};
 
 impl<'a> Display for IdentGen<'a> {
@@ -284,14 +177,29 @@ fn bytes<T: ?Sized>(x: &T) -> &[u8] {
     unsafe { core::slice::from_raw_parts(x as *const T as *const u8, core::mem::size_of_val(x)) }
 }
 
-unsafe fn replace_char(s: &mut String, start: usize, end: usize, ch: char) {
-    let len_ch = ch.len_utf8();
-    let mut len = end - start;    
+fn get_last_char(x: &str) -> char {
+    x.chars().last().unwrap()
+}
 
-    if len > len_ch {
-        s.as_mut_vec().drain(end - (len - len_ch)..end);
-        len = len_ch;
+fn replace_last_char(x: &mut String, c: char) {
+    let last = get_last_char(x);
+
+    let clen = c.len_utf8();
+    let llen = last.len_utf8();
+
+    let start = x.len() - llen;
+
+    unsafe {
+        let a = x.as_mut_vec();
+
+        let new_len = if clen > llen {
+            a.reserve((clen - llen).next_power_of_two());
+            a.len() + (clen - llen)
+        } else {
+            a.len() - (llen - clen)
+        };
+
+        a.set_len(new_len);
+        a.splice(start.., bytes(&c).iter().copied().filter(|a| *a != 0));
     }
-
-    s.as_mut_vec().splice(start..end, bytes(&ch).iter().copied().take(len));
 }
